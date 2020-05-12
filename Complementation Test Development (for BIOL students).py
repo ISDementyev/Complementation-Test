19269713d180190e3718b98888b70521d3c3c12b
ISDementyev <65086972+ISDementyev@users.noreply.github.com>
2020-05-11 21:52:53 -0400

    Uploaded new code from Jupyter file
    New code has been uploaded, outputs exist but are not yet correct. Removed test code, it can still be found in the original Jupyter file.

--- Complementation Test Development (for BIOL students).py
+++ Complementation Test Development (for BIOL students).py
@@ -1,56 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# PLEASE COMMENT ANY CHANGES NOT MADE BY THE PROJECT LEADER
 
-# PLEASE COMMENT ANY CHANGES, BOTH IN-LINE AND IN THE "COMMIT CHANGES" SECTION
-
-# IGNORE THE FOLLOWING, IT IS A TESTING BOX
-import numpy as np
-import math as mt
-
-cTest = [[1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0], 
-        [0, 0, 0, 0, 1]]
-
-print(cTest)
-
-
-# In[3]:
-
-
-trial = cTest[1, 3]
-
-
-# In[24]:
-
-
-trial = cTest[0][0]
-print(trial)
-
-trial2 = cTest[1][1]
-
-if trial == trial2:
-    print('indices')
-    
-test2 = np.arange(6).reshape(2, 3)
-
-print(test2)
-print(test2[0][1])
-
-len(test2) # finds the number of rows in the array
-
-
-# In[20]:
 
 
 ## THIS IS THE MAIN DEVELOPMENT "BOX" ##
 
-
 # Default settings + input
-cTestInput = ([1, 0, 1], [0, 1, 0], [0, 0, 1])
+cTestInput = ([1, 0, 1], [0, 1, 0], [1, 0, 1])
 groups = []
 groupRow = 0
 valid = True
@@ -60,7 +18,7 @@ j = 0
 # The input must have the identity matrix; the following code checks if this is the case.
 # It also checks to make sure m = n (number of rows = number of columns)
 
-while i <= len(cTestInput):
+while i <= len(cTestInput)-1:
     if cTestInput[i][i] != 1: # I matrix check
         valid = False
         i = i + 1  
@@ -74,97 +32,53 @@ while i <= len(cTestInput):
 i = 0
 j = 0
 
-while i <= len(cTestInput)-1:   # do we need the -1?    
-        for c in cTestInput[i]:
-                if cTestInput[i][c] not in [0, 1]:
-                        valid = False
-                elif cTestInput[i][c] == 1:
-                        groups.append(ic)
-                else: break
-    
-        if cTestInput[i][j] not in [0, 1]:
+while i <= len(cTestInput)-1:   # do we need the -1?   # indentation is not optimal but not a big issue
+        c = 0  
+        while c <= len(cTestInput[i])-1:
+            if cTestInput[i][c] not in [0, 1]:
                 valid = False
-                i = i + 1
-        
-        elif cTestInput[i][j] == 1:    # this statement "and (i != j):" should not be here, if i=j and the input is 1, its still a valid mutation group
-                groups.append(ij)
-                i = i + 1
+                c = c + 1
+                    
+            elif cTestInput[i][c] == 1:
+                mutGroupCol = [i, c]   
+                if mutGroupCol not in groups:
+                    groups.append(mutGroupCol)
+                    c = c + 1
+                else: c = c + 1                
+                
+            else: c = c + 1
+                
+        if cTestInput[i][j] not in [0, 1]:
+            valid = False
+            i = i + 1
+         
+        elif cTestInput[i][j] == 1:
+            mutGroupRow = [i, j]
+    
+            if mutGroupRow not in groups:
+                groups.append(mutGroupRow)
+            else: i = i + 1
+                        
+            i = i + 1
         
         elif cTestInput[i][j] == 0:
-                i = i + 1
-        
+                i = i + 1 
+                    
         else: i = i + 1
     
 
 # Exit Loop: now it's time to make post-appended array modifications ###################################
 # mu = mutations
 
-for mu in groups: # still haven't figured out how best to write this
-        m = mu-
-        if groups(mu)
+#for mu in groups: # still haven't figured out how best to write this
+#        m = mu-
+#        if groups(mu)
+ 
 
-        
-        
 # Finally, either we have an answer or made an error
 
 if valid == True:
     print(len(groups))
+    print(groups)
 else: 
     print('ERROR: CHECK INPUT')
-    
-
-
-# In[7]:
-
-
-# IGNORE, this is a testing box. 
-
-cTestInput = ([1, 0, 1], [0, 1, 0], [1, 0, 1])
-
-print(len(cTestInput))
-
-x = 0
-
-if x != (1 or 2):
-    print('Task failed successfully')
-else: print('task failed')
-
-
-# In[14]:
-
-
-x = [1, 2, 3, 4]
-
-for m in 0:len(x)
-print(x[m])
-
-
-# In[19]:
-
-
-cTestInput = ([1, 0, 1], [0, 1, 0], [0, 0, 1])
-
-cTestInput[2][1]
-
-i = 0
-
-valid = True
-
-while i <= len(cTestInput)-1:
-    if cTestInput[i][i] == 0:
-        valid = False
-        i = i + 1
-    else: 
-        i = i + 1
-
-if valid == False:
-    print('Error')
-else: 
-    print('Identity matrix exists')
-
-
-# In[ ]:
-
-
-
-
